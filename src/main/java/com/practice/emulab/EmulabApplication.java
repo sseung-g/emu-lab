798bea49f0db7fdcded43cb12ae4c55648a98b10
SeungGyeong <tpsseung@gmail.com>
2025-05-18 22:52:22 +0900

    Initial commit

---
+++ src/main/java/com/practice/emulab/EmulabApplication.java
@@ -0,0 +1,13 @@
+package com.practice.emulab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmulabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmulabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/emulab/EmulabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.emulab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmulabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
